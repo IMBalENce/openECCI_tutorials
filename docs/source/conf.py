3dabae63e669c7e21fe149bde4abc093a5f20193
IMBalENce <zhou.xu@monash.edu>
2024-05-14 10:47:13 +1000

    Change more webpage information.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'openECCI'
+copyright = '2024, Zhou XU'
+author = 'Zhou XU'
 
 release = '0.1'
 version = '0.1.0'
